5546a8545b52fff4aa6d428f53c2faa5bab0a1ee
Y.sh <407603405@qq.com>
2019-09-27 15:51:00 +0800

    second commit

--- src/main/java/com/example/testgit/TestgitApplication.java
+++ src/main/java/com/example/testgit/TestgitApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestgitApplication {
 
     public static void main(String[] args) {
-        System.out.println("第一次提交");
         SpringApplication.run(TestgitApplication.class, args);
     }
 }
--- src/main/java/com/example/testgit/web/HelloController.java
+++ src/main/java/com/example/testgit/web/HelloController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "web")
 public class HelloController {
 
+    /**
+    * @Description  git2.0
+    * @Author  Y.sh
+    * @Date  2019/9/27 15:49
+    */
     @RequestMapping(value = "hello")
     public String hello(){
+        System.out.println("第二次提交");
         return "hello world";
     }
 }
