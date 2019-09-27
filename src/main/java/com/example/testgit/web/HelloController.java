fa26b06f0aace9873cb3d5fc4d9c43b2fd634778
Y.sh <407603405@qq.com>
2019-09-27 16:24:59 +0800

    third commit

--- src/main/java/com/example/testgit/web/HelloController.java
+++ src/main/java/com/example/testgit/web/HelloController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     /**
-    * @Description  git2.0
+    * @Description  git3.0
     * @Author  Y.sh
     * @Date  2019/9/27 15:49
     */
     @RequestMapping(value = "hello")
     public String hello(){
-        System.out.println("第二次提交");
+        System.out.println("第 三 次提交");
         return "hello world";
     }
 }
