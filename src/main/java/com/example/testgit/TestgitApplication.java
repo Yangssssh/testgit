993d76071b1c7550b92a62baccbeecdec8456ae9
Y.sh <407603405@qq.com>
2019-09-27 11:14:29 +0800

    first commit

--- src/main/java/com/example/testgit/TestgitApplication.java
+++ src/main/java/com/example/testgit/TestgitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestgitApplication {
 
     public static void main(String[] args) {
+        System.out.println("第一次提交");
         SpringApplication.run(TestgitApplication.class, args);
     }
 }
