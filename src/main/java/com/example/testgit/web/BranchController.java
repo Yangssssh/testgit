f0f8dcd6dfcf65f2f30ed4059603ef257b327524
Y.sh <407603405@qq.com>
2019-09-29 10:52:38 +0800

    bug修改

--- src/main/java/com/example/testgit/web/BranchController.java
+++ src/main/java/com/example/testgit/web/BranchController.java
@@ -4,6 +4,10 @@ package com.example.testgit.web;
  * @author Y.sh
  * @date 2019/9/27 17:09
  * @Description  第二个分支
+ *
+ *
+ *
  */
 public class BranchController {
+
 }
---
+++ src/main/java/com/example/testgit/web/test.java
@@ -0,0 +1,9 @@
+package com.example.testgit.web;
+
+/**
+ * @author Y.sh
+ * @date 2019/9/29 10:47
+ * @Description
+ */
+public class test {
+}
